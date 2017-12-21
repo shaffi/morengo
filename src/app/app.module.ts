c4cae310ef9fac933fb65e7865c3af4fd45aca9d
Shaffiullah G <shaffiullah.g@causeway.com>
2017-12-21 00:30:43 +0000

    Add Angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -9,6 +11,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 
+import { MatCompModule } from './mat-comp/mat-comp.module';
 
 @NgModule({
   declarations: [
@@ -17,8 +20,10 @@ import { RouterModule } from '@angular/router';
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production}),
-    RouterModule
+    BrowserAnimationsModule,
+    // ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production}),
+    RouterModule,
+    MatCompModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mat-comp/mat-comp.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatButtonModule, MatCheckboxModule, MatToolbarModule} from '@angular/material';
+
+@NgModule({
+  imports: [CommonModule, MatButtonModule, MatCheckboxModule, MatToolbarModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatToolbarModule],
+  declarations: []
+})
+export class MatCompModule { }
